2cd319350cedb9511a9dfab6ee7d5b8a43d31cda
EvaMartinC <evamartincuevas@gmail.com>
2025-04-24 11:55:50 +0200

    Fixed error in loop and in the adj matrix

--- adjmatrix.py
+++ adjmatrix.py
@@ -1,3 +1,6 @@
+from itertools import permutations
+import numpy as np 
+import networkx as nx
 
 def asignar_nodos(eje_a_nodos, i, nodo_id):
     """ Asigna nodos a un segmento del tipo 2."""
@@ -33,25 +36,27 @@ def clasificar_vecinos(array_connected_branches, i):
 
 def actualizar_nodos(eje_a_nodos, group, nodo):
     """ Actualiza los nodos de los segmentos en un grupo."""
-    for segment in group[0]:
-        n1, n2 = eje_a_nodos[segment]
-        if n1 != -1 and n2 != -1:
-            continue
-        elif n1 != -1 and n1 != nodo:
-            eje_a_nodos[segment] = [n1, nodo]
-        elif n2 != -1 and n2 != nodo:
-            eje_a_nodos[segment] = [nodo, n2]
-        else:
-            eje_a_nodos[segment] = [nodo, -1]
+    if len(group) >= 1:       
+        for segment in group[0]:
+            n1, n2 = eje_a_nodos[segment]
+            if n1 != -1 and n2 != -1:
+                continue
+            elif n1 != -1 and n1 != nodo:
+                eje_a_nodos[segment] = [n1, nodo]
+            elif n2 != -1 and n2 != nodo:
+                eje_a_nodos[segment] = [nodo, n2]
+            else:
+                eje_a_nodos[segment] = [nodo, -1]
+    else:
+        print("possible mistake in graph")
 
 def procesar_segmentos(array_connected_branches, seg_type):
     """ Procesa los segmentos y asigna nodos."""
     m = len(array_connected_branches)
     eje_a_nodos = {i: [-1, -1] for i in range(m)}
     nodo_id = 1
-    
     for i in range(m):
-        if seg_type[seg_type.index[i]] == 2:
+        if seg_type[i] == 2:
             nodo_id = asignar_nodos(eje_a_nodos, i, nodo_id)
             group1, group2 = clasificar_vecinos(array_connected_branches, i)
             actualizar_nodos(eje_a_nodos, group1, eje_a_nodos[i][0])
@@ -59,4 +64,34 @@ def procesar_segmentos(array_connected_branches, seg_type):
         else:
             eje_a_nodos[i] = [eje_a_nodos[i][0],nodo_id]
             nodo_id += 1
+    for v in eje_a_nodos.values():
+        if -1 in v:
+            v[v == -1] = nodo_id
     return eje_a_nodos
+
+
+def find_adj_matrix(Gw,Gw2):
+    AM1= nx.adjacency_matrix(Gw,nodelist=list(range(1,len(Gw)+1))).toarray()
+    AM2= nx.adjacency_matrix(Gw2,nodelist=list(range(1,len(Gw2)+1))).toarray()
+    n = len(AM1)
+    best_perm = None
+    min_cost = float('inf')
+
+    # Probar todas las permutaciones posibles de nodos
+    for perm in permutations(range(n)):  
+        #print(perm)
+        # Reordenar filas y columnas de A2 según la permutación
+        AM2_permuted = AM2[np.ix_(perm, perm)]
+        
+        # Calcular la diferencia de pesos
+        cost = np.sum(np.abs(AM1 - AM2_permuted))
+        # Guardar la mejor permutación
+        if cost < min_cost:
+            min_cost = cost
+            best_perm = perm
+            
+    best_AM2 = AM2[np.ix_(best_perm, best_perm)]
+    # Imprimir la mejor asignación de nodos
+    node_mapping = {i: best_perm[i] for i in range(n)}
+    #print("Asignación óptima de nodos:", node_mapping)
+    return AM1,best_AM2
